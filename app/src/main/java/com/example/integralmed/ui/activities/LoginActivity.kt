2094fc490451cc7b43e2eb44137792c91a91f8ae
klementev <uladzislau.kliamentsyeu@gmail.com>
2022-05-18 16:02:13 +0300

    create validate data in LoginActivity.kt and RegisterActivity.kt

--- app/src/main/java/com/example/integralmed/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/integralmed/ui/activities/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
@@ -20,6 +21,23 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
         button_login.setOnClickListener(this)
     }
 
+    private fun validateLoginDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(edit_text_email.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.error_email), true)
+                false
+            }
+            TextUtils.isEmpty(edit_text_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.error_password), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar(resources.getString(R.string.error_details_valid), false)
+                true
+            }
+        }
+    }
+
 
     override fun onClick(view: View?) {
         if (view != null) {
@@ -27,6 +45,9 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
                 R.id.text_view_register -> {
                     val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
                     startActivity(intent)
+                }
+                R.id.button_login -> {
+                    validateLoginDetails()
                 }
             }
         }
--- app/src/main/java/com/example/integralmed/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/integralmed/ui/activities/RegisterActivity.kt
@@ -1,12 +1,81 @@
 package com.example.integralmed.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import com.example.integralmed.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        checkbox_terms_and_condition.setOnClickListener(this)
+        button_registration.setOnClickListener(this)
+        text_view_register_login.setOnClickListener(this)
+
+    }
+
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(
+                edit_text_registration_first_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.error_first_name), true)
+                false
+            }
+            TextUtils.isEmpty(
+                edit_text_registration_last_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.error_last_name), true)
+                false
+            }
+            TextUtils.isEmpty(edit_text_registration_email.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.error_email), true)
+                false
+            }
+            TextUtils.isEmpty(
+                edit_text_registration_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.error_password), true)
+                false
+            }
+            TextUtils.isEmpty(
+                edit_text_registration_confirm_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.error_confirm_password), true)
+                false
+            }
+            edit_text_registration_password.text.toString()
+                .trim { it <= ' ' } != edit_text_registration_confirm_password.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar(resources.getString(R.string.error_password_mismatch), true)
+                false
+            }
+            !checkbox_terms_and_condition.isChecked -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.error_check_box_terms_and_condition),
+                    true
+                )
+                false
+            }
+            else -> {
+                showErrorSnackBar(resources.getString(R.string.error_details_valid), false)
+                true
+            }
+        }
+    }
+
+    override fun onClick(view: View?) {
+        if (view != null) {
+            when (view.id) {
+                R.id.button_registration -> {
+                    validateRegisterDetails()
+                }
+                R.id.text_view_register_login -> {
+                    onBackPressed()
+                }
+            }
+        }
     }
 }
