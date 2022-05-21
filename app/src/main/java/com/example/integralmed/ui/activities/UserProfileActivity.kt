c825877d5750d6acdff49541ed6d9cfb61fd1505
klementev <uladzislau.kliamentsyeu@gmail.com>
2022-05-21 17:29:28 +0300

    Create activity_user_profile.xml

--- app/src/main/java/com/example/integralmed/ui/activities/UserProfileActivity.kt
+++ app/src/main/java/com/example/integralmed/ui/activities/UserProfileActivity.kt
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.integralmed.R
+import kotlinx.android.synthetic.main.activity_user_profile.*
 
 class UserProfileActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+
+        edit_text_user_profile_first_name
+        edit_text_user_profile_last_name
+        edit_text_user_profile_phone
+        button_user_profile_save.setOnClickListener(this)
+
     }
 
     override fun onClick(p0: View?) {
