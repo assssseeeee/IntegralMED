073b2d39654594052582005c244ee9de13e46249
klementev <uladzislau.kliamentsyeu@gmail.com>
2022-05-21 14:51:41 +0300

    sending registration data to the firestore and creating an activity UserProfileActivity.kt for editing user data

--- app/src/main/java/com/example/integralmed/firestore/FirestoreClass.kt
+++ app/src/main/java/com/example/integralmed/firestore/FirestoreClass.kt
@@ -2,10 +2,13 @@ package com.example.integralmed.firestore
 
 import android.util.Log
 import com.example.integralmed.models.User
+import com.example.integralmed.ui.activities.LoginActivity
 import com.example.integralmed.ui.activities.RegisterActivity
 import com.example.integralmed.utils.Constants
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class FirestoreClass {
     private val myFirestore = FirebaseFirestore.getInstance()
@@ -20,5 +23,9 @@ class FirestoreClass {
                 activity.hideProgressDialog()
                 Log.e(activity.javaClass.simpleName, "Error while register with user", e)
             }
+    }
+
+    fun getUserDetails(loginActivity: LoginActivity) {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/example/integralmed/ui/activities/UserProfileActivity.kt
@@ -0,0 +1,17 @@
+package com.example.integralmed.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.integralmed.R
+
+class UserProfileActivity : BaseActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+}
